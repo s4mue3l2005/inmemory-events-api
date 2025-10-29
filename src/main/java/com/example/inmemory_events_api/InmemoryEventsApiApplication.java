162fc553a9a4a3e6f620dfa676784e318d433e28
samuel Arena <samyarena2005@gmail.com>
2025-10-28 19:44:37 -0500

    Primer commit

---
+++ src/main/java/com/example/inmemory_events_api/InmemoryEventsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.inmemory_events_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmemoryEventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmemoryEventsApiApplication.class, args);
+	}
+
+}
